070e713ed6a259f41116f71e099b6caacb4666f7
OuiOuiCroissant <50111424+OuiOuiCroissant@users.noreply.github.com>
2019-10-30 14:39:50 +0200

    new features

--- features/per_menu.js
+++ features/per_menu.js
@@ -5,16 +5,20 @@ module.exports = function(controller){
       await bot.reply(message,{
           text: 'Lets start',
           persistent_menu: [
-          {
-            title: "Main menu",
-            payload: "main-menu"
-          },
-          {
-            title: "Shop",
-            payload: "shop",
-            url:"https://api.bestbuy.com/click/-/8880044/pdp"
-          }
-        ]
+            {
+              call_to_actions: [
+                {
+                  title: "Main menu",
+                  payload: "main-menu"
+                },
+                {
+                  title: "Shop",
+                  payload: "shop",
+                  url:"https://api.bestbuy.com/click/-/8880044/pdp"
+                }
+              ]
+            }
+          ]
       });
   });
 
