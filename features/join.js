0b67584f527127c011f4a2c691d7c2b4a6bfd9e6
OuiOuiCroissant <50111424+OuiOuiCroissant@users.noreply.github.com>
2019-10-30 14:26:39 +0200

    new features

---
+++ features/join.js
@@ -0,0 +1,7 @@
+module.exports = function(controller) {
+
+  controller.on('channel_join', async(bot, message) => {
+    await bot.reply(message,'Welcome to my first bot!');
+});
+
+}
---
+++ features/per_menu.js
@@ -0,0 +1,21 @@
+module.exports = function(controller){
+
+  controller.hears('Get Started', 'message',  async(bot, message) => {
+
+      await bot.reply(message,{
+          text: 'Lets start',
+          persistent_menu: [
+          {
+            title: "Main menu",
+            payload: "main-menu"
+          },
+          {
+            title: "Shop",
+            payload: "shop",
+            url:"https://api.bestbuy.com/click/-/8880044/pdp"
+          }
+        ]
+      });
+  });
+
+}
