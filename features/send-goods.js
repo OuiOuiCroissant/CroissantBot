43e362053d95d78e76d6c8021e3b2f36cdd3fe88
OuiOuiCroissant <50111424+OuiOuiCroissant@users.noreply.github.com>
2019-10-30 15:28:48 +0200

    new

--- features/send-goods.js
+++ features/send-goods.js
@@ -1,22 +1,38 @@
 module.exports = function(controller) {
+  const bby = require('bestbuy')('TGp7jkZIbKOzfRTDzkofjo2O');
 
   controller.hears('Yo', 'message', async(bot, message) => {
 
-    await bot.reply(message,{
-        text:"Yo",
-        imageUrl:'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/8880/8880044_so.jpg',
-        title:'Batman',
-        subtitle:'5,99$',
-        buttons:[
-          {
-          title:'View details',
-          payload:'view-details'
-        },
-        {
-          title:'Shop now',
-          payload:'shop-now'
-        }
-        ]
+    await bby.products(`type="${ message.quick_reply.payload }"`,{show:"image,name,salePrice"}).then(function(data){
+            for(let i=0; i<5;i++)
+            {
+            var attachment = {
+                type:'template',
+                payload:{
+                    template_type:'generic',
+                    elements:[
+                        {
+                            title:` ${data.products[i].name }`,
+                            image_url:` ${data.products[i].image }`,
+                            subtitle:` ${data.products[i].salePrice }$`,
+                            buttons:[
+                                {
+                                type:'postback',
+                                title:'Buy',
+                                payload:'buy'
+                                },
+                                {
+                                    type:'postback',
+                                    title:'Add to favorite',
+                                    payload:'add-to-favorite'
+                            }
+                            ]
+                        },
+                    ]
+                }
+            };
+            bot.reply(message, {attachment: attachment,});
+
     });
   });
 }
