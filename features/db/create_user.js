4032eb7c09ea1c9afb813f6200a1cdb13a0b5d3a
OuiOuiCroissant <50111424+OuiOuiCroissant@users.noreply.github.com>
2019-11-08 14:29:08 +0200

    added database funcs

--- features/db/create_user.js
+++ features/db/create_user.js
@@ -6,7 +6,7 @@ module.exports = function createUser(senderId) {
 
   const User = mongoose.model('User', user_schema, 'Users');
 
-  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+  mongoose.connect(MongoURI, { useNewUrlParser: true });
 
   User.create({ fbId: senderId }, (err, res) => {
     if (err) throw err;
