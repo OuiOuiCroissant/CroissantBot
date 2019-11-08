0cf356187259a26f58e07deeda64de5dccac8d22
OuiOuiCroissant <50111424+OuiOuiCroissant@users.noreply.github.com>
2019-11-08 14:28:21 +0200

    added database funcs

--- features/db/create_user.js
+++ features/db/create_user.js
@@ -2,6 +2,8 @@ module.exports = function createUser(senderId) {
   const mongoose = require('mongoose');
   const user_schema = require('./user_schema.js');
 
+  let MongoURI = 'mongodb+srv://customer:chatbots@cluster0-qgrmy.mongodb.net/botvlasenko?retryWrites=true&w=majority'
+
   const User = mongoose.model('User', user_schema, 'Users');
 
   mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
